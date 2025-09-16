6ad77c8802f6e69d1e415c6cce006f67c9eacb5f
Daivd <drgdeved@gmail.com>
2025-09-16 23:07:43 +0300

    Explain in pseudocode what to do in logic.js

---
+++ logic.js
@@ -0,0 +1,24 @@
+// program logic:
+/*
+    create an object{
+        array of input numbers and operators
+        mode //number, negative, decimal
+    }
+
+    event listener for all buttons
+        if the button was a number or an operator
+            store it in the array
+            update the screen
+
+        else, check which button
+            execute it's function
+            update the screen
+
+
+    create sum function
+        using reduce on the array
+            let float(sum) = 0;
+            if the x is a number; sum * 10 + number
+            else if x is . ; sum = sum
+
+*/
