42c5f974810f188bb1848dbe0f626d119716a382
Daivd <drgdeved@gmail.com>
2025-09-19 21:40:33 +0300

    Store numeric input chunks (with decimal support)

--- logic.js
+++ logic.js
@@ -24,6 +24,31 @@
 */
 
 let input = {
-    onScreen,
-    mode, //positive, negative
-}
+    chunk: "",
+    decimal: false,
+    mode: "positive", //positive, negative
+};
+let toCalculate = [];
+
+let calculatorScreen = document.querySelector("#calculator-screen");
+let buttons = document.querySelector("#calculator-buttons");
+
+buttons.addEventListener("click", (e)=> {
+    if (e.target.textContent >= 0 || e.target.textContent == "." && input.decimal == false) {
+        if (e.target.textContent >= 0) {
+            input.chunk += e.target.textContent
+            //updateScreen
+        }
+        else if(input.chunk == "" && e.target.textContent == ".") {
+            input.chunk = "0.";
+            input.decimal = true;
+            //updateScreen
+        }
+        else {
+            input.chunk += "."
+            input.decimal = true;
+            //updateScreen
+        }
+        
+    }
+})
